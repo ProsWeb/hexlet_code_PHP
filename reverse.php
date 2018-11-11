68b5f6367a0e10a82d3162512fb9e68f930c4a8f
ProsWeb <pros15@mail.ru>
2018-11-11 22:06:39 +0500

    add

---
+++ reverse.php
@@ -0,0 +1,16 @@
+<!-- Реализуйте функцию reverse, которая переворачивает строку.
+
+Пример:
+
+reverse('hello, world!'); // !dlrow ,olleh -->
+
+function reverse($str)
+{
+    $result = '';
+    $i = 1;
+    while ($i <= strlen($str)) {
+        $result = "{$result}{$str[strlen($str) - $i]}";
+        $i += 1;
+    }
+    return $result;
+}
