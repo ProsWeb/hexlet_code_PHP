56dcb87e2e7a3732266ceca7861124854b3c347b
ProsWeb <pros15@mail.ru>
2019-01-22 21:46:35 +0500

    fix

--- swap.php
+++ swap.php
@@ -47,7 +47,6 @@ function swap($array, $index)
     if (!array_key_exists($index + 1, $array) || !array_key_exists($index - 1, $array)) {
         return $array;
     }
-    $result = [];
     for ($i = 0; $i < sizeof($array); $i++) {
         $temp = $array[$index - 1];
         $array[$index - 1] = $array[$index + 1];
